ea40b808cc06589ad607a4b1800ac561daf24596
BijianWu <384318185@qq.com>
2022-02-08 23:05:48 +0000

    make description text pulsing when visible

--- src/DescriptionText.ts
+++ src/DescriptionText.ts
@@ -1,9 +1,14 @@
 import { Container, Text } from "pixi.js";
 import IRenderable from "./IRenderable";
+import gsap from "gsap";
 
 export default class DescriptionText implements IRenderable{
+    private readonly _scaleAmount: number = 1.2;
+
     private _text: Text;
     private _container: Container;
+
+    private _pulsingGSAP: gsap.core.Tween | undefined;
     public constructor(title: string, xPos: number, yPos: number){
         this._container = new Container();
         this._text = new Text('Click Spin To start', { fill: 0xfffb00 });
@@ -14,11 +19,25 @@ export default class DescriptionText implements IRenderable{
 
     setToVisible = () => {
         this._container.visible = true;
+        this.startPulsingTheText();
     };
 
     setToInvisible = () => {
         this._container.visible = false;
+        this.stopPulsingTheText();
     };
+
+    private startPulsingTheText = () => {
+        this._pulsingGSAP = gsap.fromTo(this._text.scale, {x : 1, y : 1}, {x: this._scaleAmount, y: this._scaleAmount, repeat: -1, yoyo: true})
+    }
+
+    private stopPulsingTheText = () => {
+        if(this._pulsingGSAP){
+            this._pulsingGSAP.kill();
+            this._pulsingGSAP = undefined;
+        }
+    }
+
 
     public getContainer = ()=>{
         return this._container;
